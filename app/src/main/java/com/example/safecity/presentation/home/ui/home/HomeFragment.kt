628d38b39a1aeaf9ba26d0448d39e8a2ae679225
Shamil <kun.shamil@gmail.com>
2022-11-19 22:11:47 +0300

    Добавил функциональность добавления маркера при нажатии

--- app/src/main/java/com/example/safecity/presentation/home/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/safecity/presentation/home/ui/home/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.safecity.presentation.home.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.safecity.databinding.FragmentHomeBinding
@@ -12,10 +14,30 @@ import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.runtime.image.ImageProvider
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var map : MapObjectCollection
+
+
+    private val  inputListener : InputListener = object : InputListener {
+        override fun onMapTap(p0: Map, p1: Point) {
+            map.addPlacemark(p1)
+            Log.d("AAAA",p1.toString())
+        }
+
+        override fun onMapLongTap(p0: Map, p1: Point) {
+
+
+
+        }
+
+    }
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -44,8 +66,15 @@ class HomeFragment : Fragment() {
             Animation(Animation.Type.SMOOTH,0f),
             null
         )
+
+        map = binding.mapview.map.mapObjects
+
+
+
+        binding.mapview.map.addInputListener(inputListener)
         return root
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         MapKitFactory.initialize(requireContext())
